9d7561adabbe0380085594a017198666ef5a2f54
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2017-05-06 09:25:15 +0200

    Main version

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-const searchForm = document.querySelector('.search-form');
-const mapElem = document.getElementById('map');
-const questionForm = document.querySelector('.question-form');
-const title = document.querySelector('.mdl-layout-title');
-let mainWaypoint;
-let startingPoint;
-let endPoint;
+const searchForm = document.querySelector('.place-search');
+let map;
+
+function initMap() {  // eslint-disable-line no-unused-vars
+  map = new google.maps.Map(
+    document.getElementById('map'),
+    {
+      zoom: 4,
+      center: { lat: 52.00, lng: 21.00 },
+    });
+
+  const directionsService = new (google.maps.DirectionsService)();
+  const directionsDisplay = new (google.maps.DirectionsRenderer)();
+  directionsDisplay.setMap(map);
+
+  searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const placeValue = event.target.querySelector('[name=place]').value;
+    geocodeAddress(placeValue).then((geocodedCoords) => {
+      const mainWayPoint = createMainWaypoint(geocodedCoords);
+
+      searchForm.parentElement.style.display = 'none';
+      const titleText = `All roads lead to ${placeValue}`;
+      document.title = titleText;
+
+      createPoint(map).then((startingCoords) => {
+        createPoint(map).then((endCoords) => {
+          calculateAndDisplayRoute(
+            directionsService,
+            directionsDisplay,
+            mainWayPoint,
+            startingCoords,
+            endCoords,
+          );
+        });
+      });
+    });
+  });
+}
 
 
 function geocodeAddress(address) {
@@ -25,86 +57,46 @@ function geocodeAddress(address) {
   });
 }
 
-function initMap() {  // eslint-disable-line no-unused-vars
-  searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    questionForm.classList.add('is-hidden');
-    const place = searchForm.querySelector('[name=place]').value;
-    geocodeAddress(place).then((response) => {
-      const map = onGeocodeComplete(response);
-      const directionsService = new google.maps.DirectionsService;
-      const directionsDisplay = new google.maps.DirectionsRenderer;
-      directionsDisplay.setMap(map);
-      const titleText = `All roads lead to ${place}`;
-      document.title = titleText;
-      title.innerHTML = titleText;
-      animateSnackbar(directionsService, directionsDisplay);
-      createPoint(map).then((startingCoords)=>{
-        startingPoint = startingCoords;
-        createPoint(map).then((endCoords)=>{
-          endPoint = endCoords;
-        })
-      });
-    }, (error) => {
-      alert(error);
-    });
-  });
-}
-
-function onGeocodeComplete(coords) {
-  const map = new google.maps.Map(
-    mapElem, {
-      zoom: 4,
-      center: coords,
-    });
-  mainWaypoint = new google.maps.Marker({  // eslint-disable-line no-unused-vars
+function createMainWaypoint(coords) {
+  map.setCenter(coords);
+  return new google.maps.Marker({
     position: coords,
     map,
     title: 'Place where all roads lead',
     icon: 'compass.png',
   });
-  return map;
 }
 
-function animateSnackbar(service, display) {
-  const snackbarContainer = document.querySelector('#select-points-toast');
-  snackbarContainer.MaterialSnackbar.showSnackbar({
-    message: 'Select starting & end point',
-    actionHandler: () => {
-      snackbarContainer.classList.add('is-hidden');
-      calculateAndDisplayRoute(service, display);
-    },
-    actionText: 'Done',
-    timeout: 100000,
-  });
-}
-
-function calculateAndDisplayRoute(service, display) {
-  service.route({
-    origin: {lat : startingPoint.position.lat(), lng: startingPoint.position.lng()},
-    destination: {lat: endPoint.position.lat(), lng: endPoint.position.lng()},
-    waypoints: [{location: mainWaypoint.position}],
-    travelMode: 'DRIVING'
-  }, function(response, status) {
-    if (status == 'OK') {
-      display.setDirections(response);
-    } else {
-      console.log(response);
-    }
-  });
-}
 function createPoint(map) {
-  return new Promise(function(resolve, reject) {
-  google.maps.event.addListenerOnce(map, 'click', (event) => {
+  return new Promise((resolve, reject) => {
+    google.maps.event.addListenerOnce(map, 'click', (event) => {
       resolve(
         new google.maps.Marker({
           position: {
             lat: event.latLng.lat(),
-            lng: event.latLng.lng()
+            lng: event.latLng.lng(),
           },
           map,
-        })
+        }),
+      );
+      reject(
+        'Error creating point',
       );
     });
+  });
+}
+
+function calculateAndDisplayRoute(service, display, wayPoint, startingPoint, endPoint) {
+  service.route({
+    origin: { lat: startingPoint.position.lat(), lng: startingPoint.position.lng() },
+    destination: { lat: endPoint.position.lat(), lng: endPoint.position.lng() },
+    waypoints: [{ location: wayPoint.position }],
+    travelMode: 'DRIVING',
+  }, (response, status) => {
+    if (status === 'OK') {
+      display.setDirections(response);
+    } else {
+      alert('Route not found!');
+    }
   });
 }
